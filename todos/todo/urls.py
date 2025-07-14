8a9871f23f36c0add7c6c315c6679049602bdbfe
ramuuzz <ramts3012005@gmail.com>
2025-07-14 22:12:43 +0530

    log in page start

--- todos/todo/urls.py
+++ todos/todo/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('register/', views.register, name='register'), 
-    path('login/', views.login, name='login'),
-    path('', views.home, name='home'),
+    path('', views.login, name='login'),
+    path('home/', views.home, name='home'),
     path('logout/', views.logout, name='logout'),
 ]
